04fa09b08cf0ce8566f099eb56459a3fe7161442
campbell-lucas <campbell_lucas@outlook.com>
2022-05-14 11:22:14 +0200

    Start time validator

--- event.py
+++ event.py
@@ -1,9 +1,59 @@
+from datetime import timedelta, datetime
+
+
 class Event:
-    def __init(self, title, location, start_time, duration, owner, participants):
+    def __init__(self, title, location, start_time, duration, owner, participants):
         self.title = title
         self.location = location
         self.start_time = start_time
         self.duration = duration
         self.owner = owner
         self.participants = participants
-        
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if len(str(value)):
+            self._title = value
+        else:
+            self._title = "No title"
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f'Invalid duration value, use integer or float: {value}')
+
+        if not (10 < value < 600):
+            raise ValueError(f'Too short or too long duration: {value} minutes')
+
+        self._duration = timedelta(minutes=value)
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+    @start_time.setter
+    def start_time(self, value):
+        if not isinstance(value, str):
+            raise TypeError(f'Invalid value, please do not use type string: {value}')
+
+        try:
+            parsed_date = datetime.strptime(value, '%d-%m-%Y %H:%M')
+        except ValueError:
+            raise ValueError(f'Invalid date format, use DD-MM-YYYY HH:MM, {value}')
+
+        if parsed_date < datetime.now() + timedelta(minutes=15):
+            raise ValueError(f'Not enough time to organize a meeting: {parsed_date - datetime.now()}')
+
+        self._start_time = value
+
+
+e = Event(42, '', '14-05-2022 11:29', 11, '', '')
+print(e.start_time)
